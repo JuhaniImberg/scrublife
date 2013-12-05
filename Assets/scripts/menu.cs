8d48c5a45a17aeb7fbaeac4c3a6f546f1ecd7a8c
JuhaniImberg <juhani@imberg.fi>
2013-12-05 18:36:09 +0200

    initial commit

---
+++ Assets/scripts/menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour {
+
+	float guiWidth = 640.0;
+	float guiHeight = 400.0;
+	Vector3 scale;
+
+	void OnGUI () {
+		scale.x = Screen.width/guiWidth;
+		scale.y = Screen.height/guiHeight;
+		scale.z = 1;
+	}
+}
